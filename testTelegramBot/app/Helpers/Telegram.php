b6a8f28b3e941fdb4d8ccfb66993d28590ad5b46
Rikki Monti <110960926+RikoMani84@users.noreply.github.com>
2022-11-08 16:38:30 +0500

    feat: add function sendDocument in Telegram class

--- testTelegramBot/app/Helpers/Telegram.php
+++ testTelegramBot/app/Helpers/Telegram.php
@@ -3,24 +3,28 @@
 namespace App\Helpers;
 
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
 
 class Telegram
 {
     protected $http;
-    protected $bot;
-
-    const url = 'https://api.telegram.org/bot';
-    public function __construct(Http $http, $bot)
+    public function __construct(Http $http)
     {
         $this->http=$http;
-        $this->bot=$bot;
     }
 
     public function sendMessage($chat_id, $message){
-        $this->http::post( self::url.$this->bot.'/sendMessage', [
+        return $this->http::post( 'https://api.telegram.org/bot5730394305:AAEJRueWmhbv6Hh_IQvHrrIsTx3WQw25TEk/sendMessage', [
             'chat_id'=>$chat_id,
             'text'=>$message,
             'parse_mode'=>'html'
+        ]);
+    }
+
+    public function sendDocument($chat_id, $file){
+        return $this->http::attach('document',Storage::get('/public/photo.png'), 'document.png')
+        ->post('https://api.telegram.org/bot5730394305:AAEJRueWmhbv6Hh_IQvHrrIsTx3WQw25TEk/sendDocument', [
+            'chat_id'=>$chat_id
         ]);
     }
 }
